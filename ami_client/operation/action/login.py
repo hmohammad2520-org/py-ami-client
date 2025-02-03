88e026d0b68c7a44f5123e0b99ad03574e262b50
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-02-03 17:04:52 +0330

    Action Improvements

--- ami_client/operation/action/login.py
+++ ami_client/operation/action/login.py
@@ -1,7 +1,5 @@
 from typing import Literal, Union
 
-from ..response.success import Success
-
 from ._base import Action
 
 class Login(Action):
@@ -37,7 +35,3 @@ class Login(Action):
         filtered_kwargs = {k: v for k, v in kwargs.items() if v is not None}
 
         super().__init__(Action=self._asterisk_name, **filtered_kwargs)
-
-    def send(self, client, raise_on_no_response=True) -> Success:
-        self.response: Success
-        return super().send(client, raise_on_no_response)
--- ami_client/operation/action/logoff.py
+++ ami_client/operation/action/logoff.py
@@ -1,7 +1,3 @@
-from typing import Literal, Union
-
-from ..response.success import Success
-
 from ._base import Action
 
 class Logoff(Action):
@@ -21,7 +17,3 @@ class Logoff(Action):
         filtered_kwargs = {k: v for k, v in kwargs.items() if v is not None}
 
         super().__init__(Action=self._asterisk_name, **filtered_kwargs)
-
-    def send(self, client, raise_on_no_response=False) -> Success:
-        self.response: Success
-        return super().send(client, raise_on_no_response)
