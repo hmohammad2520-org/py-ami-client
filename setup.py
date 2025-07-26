4926c4e7b0c386e881c70ddb154b31b5fd3fba22
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-07-26 11:13:08 +0330

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-ami-client',
-    version='0.1.0',
+    version='0.1.1',
     license="MIT",
     description='Python Asterisk Management Interface Client',
     author='Radin-System',
     author_email='technical@rsto.ir',
     url='https://github.com/Radin-System/py-ami-client',
     install_requires=[
-        "classmods==0.3.2",
+        "classmods==0.3.3",
     ],
     packages=find_packages(exclude=['test', 'test.*']),
     include_package_data=True,
